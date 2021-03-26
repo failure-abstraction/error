36a12790660e547db505285457c977ef69d2219a
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-26 11:45:29 +0100

    :art: refactor: Use flat exports style.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
-import Error from './Error.js';
-import IndexError from './IndexError.js';
-import KeyError from './KeyError.js';
-import NotImplementedError from './NotImplementedError.js';
-import StopIteration from './StopIteration.js';
-import SyntaxError from './SyntaxError.js';
-import TypeError from './TypeError.js';
-import ValueError from './ValueError.js';
-import customError from './customError.js';
-import extendError from './extendError.js';
-
-export {
-	Error,
-	IndexError,
-	KeyError,
-	NotImplementedError,
-	StopIteration,
-	SyntaxError,
-	TypeError,
-	ValueError,
-	customError,
-	extendError,
-};
+export {default as Error} from './Error.js';
+export {default as IndexError} from './IndexError.js';
+export {default as KeyError} from './KeyError.js';
+export {default as NotImplementedError} from './NotImplementedError.js';
+export {default as StopIteration} from './StopIteration.js';
+export {default as SyntaxError} from './SyntaxError.js';
+export {default as TypeError} from './TypeError.js';
+export {default as ValueError} from './ValueError.js';
+export {default as customError} from './customError.js';
+export {default as extendError} from './extendError.js';
